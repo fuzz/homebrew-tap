95865ffb6568a2956b3da5dc5c6c4e451105f4d4
Fuzz Leonard <cyborg@bionicfuzz.com>
2025-03-31 15:40:05 -0700

    Update cabal install command

--- Formula/clod.rb
+++ Formula/clod.rb
@@ -16,7 +16,7 @@ class Clod < Formula
   def install
     system "cabal", "v2-update"
     # Only install the main executable, not test programs
-    system "cabal", "v2-install", "--lib", "--bindir=#{bin}", "--program-suffix=", 
+    system "cabal", "v2-install", "--lib", "--program-suffix=", 
            "--installdir=#{bin}", "exe:clod"
     
     # Generate man pages directly during installation
