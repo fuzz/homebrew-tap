ae48832322bfb1622bc2906df252b1ccf578ffbb
Fuzz Leonard <cyborg@bionicfuzz.com>
2025-04-03 20:40:04 -0700

    Rubocop fixes

--- Formula/clod.rb
+++ Formula/clod.rb
@@ -1,59 +1,61 @@
+# frozen_string_literal: true
+
 class Clod < Formula
-  desc "Project file manager for Claude AI integrations"
-  homepage "https://github.com/fuzz/clod"
-  url "https://hackage.haskell.org/package/clod-0.1.14/clod-0.1.14.tar.gz" # TARBALL_URL_MARKER
-  sha256 "a8c6c8637500e0e7a6e135272831b5ed9a06b1770cdf4afe9bd502d8b0e336f8" # TARBALL_SHA256_MARKER
-  license "MIT"
-  
+  desc 'Project file manager for Claude AI integrations'
+  homepage 'https://github.com/fuzz/clod'
+  url 'https://hackage.haskell.org/package/clod-0.1.14/clod-0.1.14.tar.gz' # TARBALL_URL_MARKER
+  sha256 'a8c6c8637500e0e7a6e135272831b5ed9a06b1770cdf4afe9bd502d8b0e336f8' # TARBALL_SHA256_MARKER
+  license 'MIT'
+
   # Bottle specification - will be filled in after bottle creation
   bottle do
-    root_url "https://github.com/fuzz/clod/releases/download/v0.1.13" # BOTTLE_ROOT_URL_MARKER
+    root_url 'https://github.com/fuzz/clod/releases/download/v0.1.13' # BOTTLE_ROOT_URL_MARKER
     rebuild 1
-    sha256 cellar: :any, arm64_sequoia: "BOTTLE_SHA_PLACEHOLDER" # BOTTLE_SHA256_MARKER
+    sha256 cellar: :any, arm64_sequoia: 'BOTTLE_SHA_PLACEHOLDER' # BOTTLE_SHA256_MARKER
     # Add other platform/OS combinations as needed
     # sha256 cellar: :any, sequoia: "INTEL_SHA_PLACEHOLDER"
   end
-  
-  depends_on "cabal-install" => :build
-  depends_on "ghc" => :build
-  depends_on "libmagic"
-  depends_on "xxhash" => :build
-  depends_on "pandoc" => :recommended
+
+  depends_on 'cabal-install' => :build
+  depends_on 'ghc' => :build
+  depends_on 'libmagic'
+  depends_on 'xxhash' => :build
+  depends_on 'pandoc' => :recommended
 
   def install
-    system "cabal", "update"
-    
+    system 'cabal', 'update'
+
     # Use allow-newer flag to work around template-haskell version incompatibility
-    system "cabal", "install", "--disable-tests", "--allow-newer=template-haskell", 
-           "--program-suffix=", "--installdir=#{bin}", "exe:clod"
-    
+    system 'cabal', 'install', '--disable-tests', '--allow-newer=template-haskell',
+           '--program-suffix=', "--installdir=#{bin}", 'exe:clod'
+
     # Install man pages directly from source
-    if build.with? "pandoc"
-      # Generate man pages directly with pandoc
-      if File.exist?("#{buildpath}/man/clod.1.md")
-        system "pandoc", "-s", "-t", "man", "#{buildpath}/man/clod.1.md", "-o", "#{buildpath}/clod.1"
-        man1.install "#{buildpath}/clod.1" if File.exist?("#{buildpath}/clod.1")
-      end
-      
-      if File.exist?("#{buildpath}/man/clod.7.md")
-        system "pandoc", "-s", "-t", "man", "#{buildpath}/man/clod.7.md", "-o", "#{buildpath}/clod.7"
-        man7.install "#{buildpath}/clod.7" if File.exist?("#{buildpath}/clod.7")
-      end
-      
-      if File.exist?("#{buildpath}/man/clod.8.md")
-        system "pandoc", "-s", "-t", "man", "#{buildpath}/man/clod.8.md", "-o", "#{buildpath}/clod.8"
-        man8.install "#{buildpath}/clod.8" if File.exist?("#{buildpath}/clod.8")
-      end
+    return unless build.with? 'pandoc'
+
+    # Generate man pages directly with pandoc
+    if File.exist?("#{buildpath}/man/clod.1.md")
+      system 'pandoc', '-s', '-t', 'man', "#{buildpath}/man/clod.1.md", '-o', "#{buildpath}/clod.1"
+      man1.install "#{buildpath}/clod.1" if File.exist?("#{buildpath}/clod.1")
     end
+
+    if File.exist?("#{buildpath}/man/clod.7.md")
+      system 'pandoc', '-s', '-t', 'man', "#{buildpath}/man/clod.7.md", '-o', "#{buildpath}/clod.7"
+      man7.install "#{buildpath}/clod.7" if File.exist?("#{buildpath}/clod.7")
+    end
+
+    return unless File.exist?("#{buildpath}/man/clod.8.md")
+
+    system 'pandoc', '-s', '-t', 'man', "#{buildpath}/man/clod.8.md", '-o', "#{buildpath}/clod.8"
+    man8.install "#{buildpath}/clod.8" if File.exist?("#{buildpath}/clod.8")
   end
 
   def caveats
     <<~EOS
       Clod outputs the path to the staging directory, which you can use with:
-      
+
       # On macOS, open the staging directory in Finder:
         open `clod [options]`
-      
+
       # For scripts, you can capture the output:
         STAGING_DIR=$(clod [options])
         open "$STAGING_DIR"
@@ -61,11 +63,15 @@ class Clod < Formula
   end
 
   test do
-    assert_match "Clod - Project file manager for Claude AI", shell_output("#{bin}/clod --help")
-    
+    assert_match 'Clod - Project file manager for Claude AI', shell_output("#{bin}/clod --help")
+
     # Only test man pages if they should be installed
-    if build.with? "pandoc"
-      system "man", "-w", "clod" rescue nil
+    if build.with? 'pandoc'
+      begin
+        system 'man', '-w', 'clod'
+      rescue StandardError
+        nil
+      end
     end
   end
 end
